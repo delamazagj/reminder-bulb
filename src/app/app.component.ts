a60ee9be3170d3f86d59584c91f7252ffbd1e763
Gerardo De La Maza <delamazagj@gmail.com>
2022-10-04 00:11:41 -0400

    Initial feature schematics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { Post } from './model/post.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reminder-bulb';
+  storedPosts: Post[] = [];
+
+  onPostAdded(post: Post) {
+    this.storedPosts.push(post);
+  }
 }
