c8c16165a9d1d32870d063cdbf5ac4c5c604fb8a
Gerardo De La Maza <delamazagj@gmail.com>
2022-10-17 19:00:24 -0400

    Added image loader and mime validator

--- src/app/reminders/reminder-create/reminder-create.component.ts
+++ src/app/reminders/reminder-create/reminder-create.component.ts
@@ -1,8 +1,9 @@
 import { Reminder } from './../../model/reminder.model';
 import { RemindersService } from './../reminder.service';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-reminder-create',
@@ -15,6 +16,8 @@ export class ReminderCreateComponent implements OnInit {
   id!: string;
   reminder!: Reminder;
   isLoading = false;
+  form!: FormGroup;
+  imagePreviewUrl!: string;
 
   constructor(
     private remindersService: RemindersService,
@@ -28,12 +31,29 @@ export class ReminderCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+
     this.route.paramMap.subscribe(params => {
       this.id = params.has('id') ? (params.get('id') as string) : '';
       if (this.id) {
         this.isLoading = true;
         this.remindersService.getReminder(this.id).subscribe(res => {
           this.reminder = res.reminder;
+          this.form.setValue({
+            title: this.reminder.title,
+            content: this.reminder.content
+          });
           console.log(this.reminder);
           this.isLoading = false;
         });
@@ -41,20 +61,20 @@ export class ReminderCreateComponent implements OnInit {
     });
   }
 
-  onUpdateReminder(form: NgForm) {
-    if (form.invalid) {
+  onUpdateReminder() {
+    if (this.form.invalid) {
       return;
     }
     this.remindersService.updateReminder(
       this.reminder.id,
-      form.value.title,
-      form.value.content
+      this.form.value.title,
+      this.form.value.content
     );
-    form.resetForm();
+    this.form.reset();
   }
 
-  onSaveReminder(form: NgForm) {
-    if (form.invalid) {
+  onSaveReminder() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
@@ -63,17 +83,35 @@ export class ReminderCreateComponent implements OnInit {
       console.log('Saving normally');
       this.remindersService.addReminder(
         this.reminder.id,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     } else {
       console.log('Updating normally');
       this.remindersService.updateReminder(
         this.reminder.id,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
-    form.resetForm();
+    this.form.reset();
+  }
+
+  onImageChanged($event: Event) {
+    const inputEl = $event.target as HTMLInputElement;
+
+    let image;
+    if (inputEl.files) {
+      image = inputEl.files[0];
+
+      this.form.patchValue({ image: image });
+      this.form.get('image')?.updateValueAndValidity();
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreviewUrl = reader.result as string;
+      };
+      reader.readAsDataURL(image);
+    }
   }
 }
